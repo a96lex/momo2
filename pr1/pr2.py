fcfe98793e34813b3241ea19a236e4f2e59425d3
a96lex <a96lex@gmail.com>
2020-11-21 01:11:10 +0100

    trying to verlet

--- pr1/pr2.py
+++ pr1/pr2.py
@@ -1,12 +1,12 @@
 from functions.particle_generation import sc_lattice, fcc_lattice, write_file
 from functions.system_functions import calculate_energy, find_force
 from functions.math_functions import vector_module
+from functions.system_evolution import verlet
 
 
 particles, L = sc_lattice(216, 0.8)
-energy_vector = calculate_energy(particles, L / 2, L)
 energy_matrix = find_force(particles, L / 2, L)
-print("sc lattice for " + str(len(particles)) + " particles")
-print("Lennard-Jones potential energy (with perdiodic boundary conditions)")
-print("cutoff: ", L / 2, " energy: ", vector_module(energy_vector))
-write_file(particles, "test")
+print(energy_matrix)
+
+particles, particles2, vel = verlet(particles, particles, 0.1, L, L / 2)
+energy = calculate_energy(particles, L / 2, L)
