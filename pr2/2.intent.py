f74eeaf91eda7a9caed890e8b012846e323bafb9
a96lex <a96lex@gmail.com>
2020-11-25 12:21:55 +0100

    surten coses

--- pr2/2.intent.py
+++ pr2/2.intent.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 
 part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
 part2 = np.copy(part1)
-L = 3
-dt = 0.0001
+L = 10
+dt = 0.001
+iteracions = 10000
+m = 1
 
 
 def find_force_LJ0(r, L_box, cutoff):
@@ -24,6 +26,7 @@ def find_force_LJ0(r, L_box, cutoff):
                 F[i][0] = F[i][0] + (48 / d ** 14 - 24 / d ** 8) * dx
                 F[i][1] = F[i][1] + (48 / d ** 14 - 24 / d ** 8) * dy
                 F[i][2] = F[i][2] + (48 / d ** 14 - 24 / d ** 8) * dz
+
                 F[j][0] = F[j][0] - (48 / d ** 14 - 24 / d ** 8) * dx
                 F[j][1] = F[j][1] - (48 / d ** 14 - 24 / d ** 8) * dy
                 F[j][2] = F[j][2] - (48 / d ** 14 - 24 / d ** 8) * dz
@@ -35,26 +38,55 @@ def find_force_LJ0(r, L_box, cutoff):
     return np.array(F), pot
 
 
-def verlet(part1, part2, dt, L, cutoff):
+def euler(part, vel1, dt, L, cutoff, m):
     forces, pot = find_force_LJ0(part1, cutoff, L)
-    part3 = np.zeros(np.shape(part1))
-    vel = np.zeros(np.shape(part1))
-    for i in range(len(part1)):
-        part3[i] = 2 * part2[i] - part1[i] + forces[i] * dt ** 2
-        vel[i] = (part2[i] - part1[i]) / (2 * dt)
+    part2 = part + vel1 * dt + 0.5 * forces * dt ** 2 / m
+    vel2 = vel1 + forces * dt / m
+    return part2, vel2, pot
+
+
+def verlet(part1, part2, dt, L, cutoff, m):
+    forces, pot = find_force_LJ0(part1, cutoff, L)
+    part3 = 2 * part2 - part1 + forces * dt ** 2 / m
     return part2, part3, pot
+
+
+def vel_verlet(part, vel1, dt, L, cutoff, m):
+    forces, pot = find_force_LJ0(part1, cutoff, L)
+    part2 = part + vel1 * dt + 0.5 * forces * dt ** 2 / m
+    vel2 = vel1 + forces * 0.5 * dt
+    return part2, vel2, pot
 
 
 test1 = []
 test2 = []
-it = []
-for i in range(100000):
-    part1, part2, pot = verlet(part1, part2, dt, L, L / 2)
-    test1.append(part1[0][1])
-    test2.append(part2[0][1])
-    it.append(i)
+test3 = []
+t = np.arange(0, iteracions) * dt
+vel = np.zeros(np.shape(part1))
+pot1 = []
+pot2 = []
+pot3 = []
+for i in range(iteracions):
+    part1, vel, pot = euler(part1, vel, dt, L, L / 2, m)
+    test1.append(abs(part1[0][1] - part1[1][1]))
+    pot1.append(pot)
+
+part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
+vel = np.zeros(np.shape(part1))
+for i in range(iteracions):
+    part1, vel, pot = vel_verlet(part1, vel, dt, L, L / 2, m)
+    test2.append(abs(part1[0][1] - part1[1][1]))
+    pot2.append(pot)
+
+part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
+for i in range(iteracions):
+    part1, part2, pot = vel_verlet(part1, part2, dt, L, L / 2, m)
+    test3.append(abs(part1[0][1] - part1[1][1]))
+    pot3.append(pot)
 
 
-plt.plot(it, test1)
-plt.plot(it, test2)
+plt.plot(t, test1, label="euler")
+plt.plot(t, test2, label="verlet vel")
+plt.plot(t, test3, label="verlet")
+plt.legend()
 plt.show()
