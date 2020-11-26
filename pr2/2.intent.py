a0d59cf78d3d1cd82e785d380607debead8c2901
a96lex <a96lex@gmail.com>
2020-11-26 23:38:02 +0100

    updated plots

--- pr2/2.intent.py
+++ pr2/2.intent.py
@@ -1,92 +1,63 @@
 import numpy as np
-from funcions.system_evolution import verlet
+from funcions.write_xyz import write_file
+from funcions.system_functions import find_force_LJ0, vel_verlet, verlet, euler
 import matplotlib.pyplot as plt
 
-part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
+part1 = np.array([[1, 1.1, 0], [0, 0, 0]])
 part2 = np.copy(part1)
 L = 10
-dt = 0.001
-iteracions = 10000
+dt = 0.0003
+iteracions = 30000
 m = 1
 
 
-def find_force_LJ0(r, L_box, cutoff):
-    N = len(r)
-    F = np.zeros((N, 3))
-    pot = 0.0
-    for i in range(N):
-        for j in range(i + 1, N):
-            dx = r[i][0] - r[j][0]
-            dy = r[i][1] - r[j][1]
-            dz = r[i][2] - r[j][2]
-            d = (
-                dx ** 2 + dy ** 2 + dz ** 2
-            ) ** 0.5  # Calculating distance between particles i and j
-            if d < cutoff:
-                F[i][0] = F[i][0] + (48 / d ** 14 - 24 / d ** 8) * dx
-                F[i][1] = F[i][1] + (48 / d ** 14 - 24 / d ** 8) * dy
-                F[i][2] = F[i][2] + (48 / d ** 14 - 24 / d ** 8) * dz
-
-                F[j][0] = F[j][0] - (48 / d ** 14 - 24 / d ** 8) * dx
-                F[j][1] = F[j][1] - (48 / d ** 14 - 24 / d ** 8) * dy
-                F[j][2] = F[j][2] - (48 / d ** 14 - 24 / d ** 8) * dz
-        pot = (
-            pot
-            + 4.0 * (1 / d ** 12 - 1 / d ** 6)
-            - 4.0 * (1 / cutoff ** 12 - 1 / cutoff ** 6)
-        )
-    return np.array(F), pot
+pose = []
+posv = []
+posvv = []
+pote = []
+potv = []
+potvv = []
+cine = []
+cinv = []
+cinvv = []
+enee = []
+enev = []
+enevv = []
 
 
-def euler(part, vel1, dt, L, cutoff, m):
-    forces, pot = find_force_LJ0(part1, cutoff, L)
-    part2 = part + vel1 * dt + 0.5 * forces * dt ** 2 / m
-    vel2 = vel1 + forces * dt / m
-    return part2, vel2, pot
-
-
-def verlet(part1, part2, dt, L, cutoff, m):
-    forces, pot = find_force_LJ0(part1, cutoff, L)
-    part3 = 2 * part2 - part1 + forces * dt ** 2 / m
-    return part2, part3, pot
-
-
-def vel_verlet(part, vel1, dt, L, cutoff, m):
-    forces, pot = find_force_LJ0(part1, cutoff, L)
-    part2 = part + vel1 * dt + 0.5 * forces * dt ** 2 / m
-    vel2 = vel1 + forces * 0.5 * dt
-    return part2, vel2, pot
-
-
-test1 = []
-test2 = []
-test3 = []
 t = np.arange(0, iteracions) * dt
 vel = np.zeros(np.shape(part1))
-pot1 = []
-pot2 = []
-pot3 = []
 for i in range(iteracions):
-    part1, vel, pot = euler(part1, vel, dt, L, L / 2, m)
-    test1.append(abs(part1[0][1] - part1[1][1]))
-    pot1.append(pot)
+    # write_file(part1, "teest")
+    part1, vel, pot, kin = euler(part1, vel, dt, L, L / 2, m)
+    pose.append(abs(part1[0][1] - part1[1][1]))
+    enee.append(pot + kin)
+    pote.append(pot)
+    cine.append(kin)
+
 
 part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
 vel = np.zeros(np.shape(part1))
 for i in range(iteracions):
-    part1, vel, pot = vel_verlet(part1, vel, dt, L, L / 2, m)
-    test2.append(abs(part1[0][1] - part1[1][1]))
-    pot2.append(pot)
+    part1, vel, pot, kin = vel_verlet(part1, vel, dt, L, L / 2, m)
+    posvv.append(abs(part1[0][1] - part1[1][1]))
+    enevv.append(pot + kin)
+    potvv.append(pot)
+    cinvv.append(kin)
+
 
 part1 = np.array([[0, 1.1, 0], [0, 0, 0]])
+part2 = np.array([[0, 1.1, 0], [0, 0, 0]])
 for i in range(iteracions):
-    part1, part2, pot = vel_verlet(part1, part2, dt, L, L / 2, m)
-    test3.append(abs(part1[0][1] - part1[1][1]))
-    pot3.append(pot)
+    part1, part2, pot, kin = verlet(part1, part2, dt, L, L / 2, m)
+    posv.append(abs(part1[0][1] - part1[1][1]))
+    enev.append(pot + kin)
+    potv.append(pot)
+    cinv.append(kin)
 
 
-plt.plot(t, test1, label="euler")
-plt.plot(t, test2, label="verlet vel")
-plt.plot(t, test3, label="verlet")
+plt.plot(t, enee, label="euler")
+plt.plot(t, enev, label="verlet")
+plt.plot(t, enevv, label="verlet vel")
 plt.legend()
 plt.show()
