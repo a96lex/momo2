399fabcd8274dca86cbdb3b564f50ac6698c6658
a96lex <a96lex@gmail.com>
2020-12-15 18:27:37 +0100

    removed unused stuff

--- pr3/pr3.py
+++ pr3/pr3.py
@@ -9,38 +9,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# ex 1
-particles, velocities, L = sc_lattice_velocity(8, 0.8442)
-
-# ex 2
-temp = 10
-nu = 1
-dt = 0.0001
-
-# ex 3
 # Initial conditios
-lattice = 4
-particles, velocities, L = sc_lattice_velocity(lattice, 0.8442)
+lattice = 3
+dt = 0.0001
 T = 1000
-n = 100
+n = 1
+
+# System generation
+particles, velocities, L = sc_lattice_velocity(lattice, 0.8442)
 velocities = 0
 temps = [0]
 temp = [T]
-
 # Setting the storage of the results
 filename = "100_2.dat"
 write_file(particles, filename, "w")
-
 # Main loop
 for i in range(n):
-    print("\n\n\n\n\n\n\n\n\nSimulation at", 100 * i / n, "%")
+    # print("\n\n\n\n\n\n\n\n\nSimulation at", 100 * i / n, "%")
     temps.append(i * dt)
-    particles, velocities, kin = time_step_vVerlet_temp(
-        particles, velocities, dt, L, T ** 0.5
+    particles, velocities, T = time_step_vVerlet_temp(
+        particles, velocities, dt, L, temp[0], lattice ** 3
     )
     write_file(particles, filename, "a")
-    temp.append((kin * 2 / (3 * lattice ** 3)))
-print("\n\n\n\n\n\n\n\n\nSimulation complete")
+    temp.append(T)
+# print("\n\n\n\n\n\n\n\n\nSimulation complete")
 
 # Plot the temperature over time
 plt.plot(temps, temp)
